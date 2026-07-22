1b0e9525d673cc89d8bb56ab8a0779298b9f1e7a
haoyangtu09-art <haoyangtu09@gmail.com>
2026-07-22 12:15:58 +0800

    Fix stable-only release CI

--- module/test-expert-relay-regression.sh
+++ module/test-expert-relay-regression.sh
@@ -6,9 +6,6 @@ OUT="build/expert-relay-test"
 rm -rf "$OUT"
 mkdir -p "$OUT/classes"
 
-cmp src/com/dsmod/relay/ExpertRelayGate.java \
-    ../module-inject-legacy/src/com/dsmod/relay/ExpertRelayGate.java
-
 javac -source 8 -target 8 -d "$OUT/classes" \
     src/com/dsmod/relay/ExpertRelayGate.java \
     tests/com/dsmod/relay/ExpertRelayGateRegressionTest.java
